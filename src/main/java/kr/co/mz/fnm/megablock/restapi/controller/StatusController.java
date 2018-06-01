85cdd71b969d1f7f4922f4001d81a149a9d0c790
erinis eligro <erinis.dev@gmail.com>
2018-06-01 20:02:46 +0900

    Initial commit

---
+++ src/main/java/kr/co/mz/fnm/megablock/restapi/controller/StatusController.java
@@ -0,0 +1,23 @@
+package kr.co.mz.fnm.megablock.restapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String helloMegablock() {
+        return "Hello Megablock!!";
+    }
+
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String isRunning() {
+        return "I'm Alive!";
+    }
+}
